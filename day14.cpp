65dc85757ef575532b1e4828a6e9a0d254aaa839
Rohini Daddekar <rohinidaddekar@gmail.com>
2025-08-28 21:59:30 +0530

    day14 challenge

---
+++ day14.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    // Count substrings with at most K distinct characters
+    int countAtMostK(string &s, int k) {
+        int n = s.size();
+        int left = 0, result = 0;
+        unordered_map<char, int> freq;
+
+        for (int right = 0; right < n; right++) {
+            freq[s[right]]++;
+
+            while ((int)freq.size() > k) { // shrink window
+                freq[s[left]]--;
+                if (freq[s[left]] == 0)
+                    freq.erase(s[left]);
+                left++;
+            }
+
+            result += (right - left + 1); 
+        }
+
+        return result;
+    }
+
+    int countExactlyK(string s, int k) {
+        return countAtMostK(s, k) - countAtMostK(s, k - 1);
+    }
+};
+
+int main() {
+    string s = "pqpqs";
+    int k = 2;
+
+    Solution sol;
+    cout << sol.countExactlyK(s, k) << endl; // Output: 7
+    return 0;
+}
